3ec944ce88f44f495518c1f1e09ef0e7b542379e
kazhashimoto <zkar.h62@gmail.com>
2021-11-22 22:34:31 +0900

    Update API. Fix duplicate injection of CSS.

--- extension/content.js
+++ extension/content.js
@@ -13,7 +13,9 @@
       }
     }
     document.body.classList.toggle(`${CLASSNAME}-active`);
-    return;
+    if (!from_extension) {
+      return;
+    }
   }
 
   options.preset = true;
@@ -31,9 +33,17 @@
   }
 
   function start() {
-    document.body.classList.add(CLASSNAME, `${CLASSNAME}-active`);
-    process(options);
+    let state = true;
+    if (document.body.classList.contains(CLASSNAME)) {
+      if (!document.body.classList.contains(`${CLASSNAME}-active`)) {
+        state = false;
+      }
+    } else {
+      document.body.classList.add(CLASSNAME, `${CLASSNAME}-active`);
+    }
+    process(options, state);
   }
+
 })('img-marker', // [1] <body>に追加するclass名
 function init_options(options) {
   // [2] オプションの初期値を設定するコードをここに書く ---
@@ -41,14 +51,21 @@ function init_options(options) {
   options.colors = {};
   // --- ここまで [2]
 },
-function(options) {
+function(options, state) {
+  console.log('### state', state);
+  if (!state) { // アイコンの状態がOFFになって呼ばれた時
+    document.querySelectorAll('iframe').forEach(e => {
+      e.classList.remove('_img-marker-iframe');
+    });
+    return;
+  }
   // [3] 機能を実装するコードの本体をここに書く ---
-  document.querySelectorAll('body *').forEach((e) => {
+  document.querySelectorAll('body *').forEach(e => {
     let color;
     if (e.localName == 'img') {
       color = 'red'
     } else if (e.localName == 'iframe') {
-      e.style.opacity = 0.1;
+        e.classList.add('_img-marker-iframe');
     } else {
       const style = window.getComputedStyle(e);
       if (style.backgroundImage.includes('url')) {
@@ -57,9 +74,10 @@ function(options) {
     }
     if (color) {
       e.style.outline = `2px solid ${color}`;
-      e.style.outlineOffset = '-2px'
-      e.classList.add('_img-marker');
+      e.style.outlineOffset = '-2px';
+      e.classList.add('_img-marker-element');
     }
   });
   // --- ここまで [3]
-});
+}
+);
